46a7f21e9c4b8223531f5ffba5cd3a019f295b1c
Max Foxhunter <max.value@hotmail.com>
2016-01-04 00:17:48 +0100

    Optimized meta.js
    in watchmode script

--- AnilinkzWatchMode/Anilinkz_Auto_Watch_Mode.meta.js
+++ AnilinkzWatchMode/Anilinkz_Auto_Watch_Mode.meta.js
@@ -1,5 +1,4 @@
 // ==UserScript==
-// @version     6.0
-// @updateURL   https://raw.githubusercontent.com/MaxValue/Greasemonkey/master/AnilinkzWatchMode/Anilinkz_Auto_Watch_Mode.meta.js
+// @version     6.1
 // @downloadURL https://raw.githubusercontent.com/MaxValue/Greasemonkey/master/AnilinkzWatchMode/Anilinkz_Auto_Watch_Mode.user.js
 // ==/UserScript==
