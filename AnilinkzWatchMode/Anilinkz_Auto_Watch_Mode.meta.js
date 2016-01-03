c85585e949e293d72ffe29dd77a48de46a67e1c2
Max Foxhunter <max.value@hotmail.com>
2016-01-04 00:22:46 +0100

    Fixing Update Issue
    first try

--- AnilinkzWatchMode/Anilinkz_Auto_Watch_Mode.meta.js
+++ AnilinkzWatchMode/Anilinkz_Auto_Watch_Mode.meta.js
@@ -1,4 +1,4 @@
 // ==UserScript==
-// @version     6.1
+// @version     7.0
 // @downloadURL https://raw.githubusercontent.com/MaxValue/Greasemonkey/master/AnilinkzWatchMode/Anilinkz_Auto_Watch_Mode.user.js
 // ==/UserScript==
