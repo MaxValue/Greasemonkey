e1b6851b762651ffd289a9391fbaffd373b3bef5
Max Foxhunter <max.value@hotmail.com>
2016-01-04 00:07:22 +0100

    Initial Upload

---
+++ AnilinkzWatchMode/Anilinkz_Auto_Watch_Mode.meta.js
@@ -0,0 +1,5 @@
+// ==UserScript==
+// @version     6.0
+// @updateURL   https://raw.githubusercontent.com/MaxValue/Greasemonkey/master/AnilinkzWatchMode/Anilinkz_Auto_Watch_Mode.meta.js
+// @downloadURL https://raw.githubusercontent.com/MaxValue/Greasemonkey/master/AnilinkzWatchMode/Anilinkz_Auto_Watch_Mode.user.js
+// ==/UserScript==
---
+++ AnilinkzWatchMode/Anilinkz_Auto_Watch_Mode.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Anilinkz Auto Watch Mode
+// @namespace   com.github.maxvalue.anilinzWatchMode
+// @description automatically sets the watch mode on anilinkz.tv pages.
+// @version     6.0
+// @updateURL   https://raw.githubusercontent.com/MaxValue/Greasemonkey/master/AnilinkzWatchMode/Anilinkz_Auto_Watch_Mode.meta.js
+// @downloadURL https://raw.githubusercontent.com/MaxValue/Greasemonkey/master/AnilinkzWatchMode/Anilinkz_Auto_Watch_Mode.user.js
+// @include     /^https?://(www\.)?anilinkz\.tv/.*$/
+// @grant       none
+// @run-at      document-idle
+// ==/UserScript==
+$('#watchmode').click();
+$('#chatango').empty();
+$('div[id^="adp_position_AD_"]')[0].style.display='none';
